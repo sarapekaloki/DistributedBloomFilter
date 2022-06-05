d9574ad65287af44729fc09b9b0827190c1fd98e
sarapekaloki <78003706+sarapekaloki@users.noreply.github.com>
2022-06-04 20:36:55 -0700

    Create read_Data.h

---
+++ include/read_Data.h
@@ -0,0 +1,17 @@
+//
+// Created by SARAMARQUEZ on 5/24/2022.
+//
+
+#ifndef FILTROBLOOM_READ_DATA_H
+#define FILTROBLOOM_READ_DATA_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../include/BloomFilter.h"
+
+std::vector<std::string> getData(int dataQuantity);
+
+
+
+#endif //FILTROBLOOM_READ_DATA_H
