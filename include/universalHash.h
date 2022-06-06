16e0fd9be63c7d1f8665992c7b317728a0499164
sarapekaloki <78003706+sarapekaloki@users.noreply.github.com>
2022-06-05 21:40:29 -0700

    Create universalHash.h

---
+++ include/universalHash.h
@@ -0,0 +1,22 @@
+//
+// Created by Sara Marquez on 04/06/22.
+//
+
+#ifndef BLOOMFILTER_UNIVERSALHASH_H
+#define BLOOMFILTER_UNIVERSALHASH_H
+
+
+#include "universalHash.h"
+#include <string>
+#include <fstream>
+using namespace std;
+#define ll long long
+
+int h1(string s, int bloomsz);
+int h2(string s, int bloomsz);
+int h3(string s, int bloomsz);
+int h4(string s, int bloomsz);
+
+
+
+#endif //BLOOMFILTER_UNIVERSALHASH_H
