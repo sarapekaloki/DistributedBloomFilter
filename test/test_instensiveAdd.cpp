9f76011bed571af6e8d288bd34fc6a3a611a6a70
sarapekaloki <78003706+sarapekaloki@users.noreply.github.com>
2022-06-05 21:43:07 -0700

    Update test_instensiveAdd.cpp

--- test/test_instensiveAdd.cpp
+++ test/test_instensiveAdd.cpp
@@ -1,55 +1,34 @@
-//
-// Created by Sara Marquez on 22/04/22.
-//
 #include <iostream>
 #include <vector>
 #include "../include/BloomFilter.h"
+#include "../include/universalHash.h"
 #include <algorithm>
 using namespace std;
-typedef int (*HashFunction)(string);
-int bloomsz=4000000;
-int hash1(string s)
-{
-    int hash = 0;
-    //Se itera a traves del string insertado
-    for (int i = 0; i < s.size(); i++)
-    {
-        //Se suma el valor de hash, a el valor decimal de una letra del string
-        hash = (hash + ((int)s[i]));
-        //Despues a ese valor se le hace mod el tamaño del filtro
-        hash = hash % bloomsz;
-    }
-    return hash;
-}
 
-// hash 3
-int hash3(string s)
-{
-    int hash = 7;
-    for (int i = 0; i < s.size(); i++)
-    {
-        hash = (hash * 31 + s[i]) % bloomsz;
-    }
-    return hash % bloomsz;
-}
+
+
 
 void intensiveAdd(){
     vector<HashFunction> v;
+    HashFunction hash1 = h1;
+    HashFunction  hash2 = h2;
     v.push_back(hash1);
-    v.push_back(hash3);
-
-    BloomFilter fltr(8000000,v);
+    v.push_back(hash2);
+    int size = 8000009;
+    int dataToInsert = 5000000;
+    BloomFilter fltr(size,v);
     int cont =0;
-    ifstream ip ("/Users/SARAMARQUEZ/Desktop/Ing Ciencias Computacionales/6to Semestre/Algoritmos/dataset/modNames.csv");
-
+    ifstream ip ("/Users/saramarquez/Documents/ICC/6TO/Analisis de algoritmos/BloomFilter/dataset/modNames.csv");
     string name;
-    while (ip.good() && cont<=5000000){
+    while (ip.good() && cont<=dataToInsert){
         getline(ip,name,'\n');
         fltr.add(name);
         cont+=1;
     }
     ip.close();
-    cout<<"INTENSIVE ADD TEST PASSED";
+    cout<<"INTENSIVE ADD TEST PASSED "<< dataToInsert << " NAMES ADDED";
     return;
 }
-
+//int main (){
+//    intensiveAdd();
+//}
