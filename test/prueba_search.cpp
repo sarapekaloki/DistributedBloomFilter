44da5e97f634a9e27fc5f4658d3de4d599517748
sarapekaloki <78003706+sarapekaloki@users.noreply.github.com>
2022-06-05 21:42:29 -0700

    Update prueba_search.cpp

--- test/prueba_search.cpp
+++ test/prueba_search.cpp
@@ -1,30 +1,17 @@
-//
-// Created by Sara Marquez on 22/04/22.
-//
 #include <iostream>
 #include <vector>
 #include "../include/BloomFilter.h"
+#include "../include/universalHash.h"
 #include <algorithm>
 using namespace std;
-typedef int (*HashFunction)(string);
 
-int hashfunc2(string s)
-{
-    int hash = 0;
-    //Se itera a traves del string insertado
-    for (int i = 0; i < s.size(); i++)
-    {
-        //Se suma el valor de hash, a el valor decimal de una letra del string
-        hash = (hash + ((int)s[i]));
-        //Despues a ese valor se le hace mod el tamaño del filtro
-        hash = hash % 20;
-    }
-    return hash;
-}
+
+
 
 void prueba_search(){
     vector<HashFunction> v;
-    v.push_back(hashfunc2);
+    HashFunction hash = h1;
+    v.push_back(hash);
 
     BloomFilter fltr(20,v);
     vector<string> names = {"sara","mario","hugo","almaraz","peka"};
